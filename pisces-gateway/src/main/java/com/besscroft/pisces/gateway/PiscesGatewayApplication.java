93c6cb75d6db34280ac9587cf5f711f73846dec7
besscroft <besscroft@foxmail.com>
2022-01-30 23:17:18 +0800

    :tada:创建 pisces-gateway 模块  - 新建网关基本模块  - 修改了项目名

---
+++ pisces-gateway/src/main/java/com/besscroft/pisces/gateway/PiscesGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.besscroft.pisces.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@Slf4j
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PiscesGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiscesGatewayApplication.class, args);
+        log.info("(♥◠‿◠)ﾉﾞ  Pisces 网关启动成功   ლ(´ڡ`ლ)ﾞ  \n");
+    }
+
+}
---
+++ pisces-gateway/src/test/java/com/besscroft/pisces/gateway/ImetaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.besscroft.pisces.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImetaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
