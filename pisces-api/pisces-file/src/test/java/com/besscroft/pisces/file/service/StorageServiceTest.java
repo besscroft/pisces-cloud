aa2e67ed23935caf0c75c510a6a8bfdebafe44ec
besscroft <besscroft@foxmail.com>
2022-08-08 14:29:23 +0800

    test:add unit test

---
+++ pisces-api/pisces-file/src/test/java/com/besscroft/pisces/file/service/StorageServiceTest.java
@@ -0,0 +1,45 @@
+package com.besscroft.pisces.file.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Description
+ * @Author Bess Croft
+ * @Date 2022/8/8 14:26
+ */
+@Slf4j
+@SpringBootTest
+public class StorageServiceTest {
+
+    @Autowired
+    private StorageService storageService;
+
+    @Test
+    @DisplayName("存放对象进存储桶方法测试")
+    void putObject() {
+
+    }
+
+    @Test
+    @DisplayName("从存储桶中获取对象方法测试")
+    void getObject() {
+
+    }
+
+    @Test
+    @DisplayName("获取对象的地址方法测试")
+    void getObjectUrl() {
+
+    }
+
+    @Test
+    @DisplayName("从存储桶中删除对象方法测试")
+    void removeObject() {
+
+    }
+
+}
