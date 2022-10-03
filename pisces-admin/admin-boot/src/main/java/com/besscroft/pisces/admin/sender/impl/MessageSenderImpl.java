02d8de592156de978c359e7e336048188387b8c9
besscroft <besscroft@foxmail.com>
2022-10-03 13:18:02 +0800

    fix:update nacos config  - update nacos config  - fix amqp error

--- pisces-admin/admin-boot/src/main/java/com/besscroft/pisces/admin/sender/impl/MessageSenderImpl.java
+++ pisces-admin/admin-boot/src/main/java/com/besscroft/pisces/admin/sender/impl/MessageSenderImpl.java
@@ -5,6 +5,7 @@ import com.besscroft.pisces.framework.common.constant.AMQPConstant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 /**
@@ -20,36 +21,43 @@ public class MessageSenderImpl implements MessageSender {
     private final AmqpTemplate rabbitTemplate;
 
     @Override
+    @Async
     public void send(String message) {
         rabbitTemplate.convertAndSend(AMQPConstant.MESSAGE_QUEUE, message);
     }
 
     @Override
+    @Async
     public void sendSMS(String message) {
         rabbitTemplate.convertAndSend(AMQPConstant.SMS_QUEUE, message);
     }
 
     @Override
+    @Async
     public void sendEmail(String email) {
         rabbitTemplate.convertAndSend(AMQPConstant.EMAIL_QUEUE, email);
     }
 
     @Override
+    @Async
     public void sendServerChan(String message) {
         rabbitTemplate.convertAndSend(AMQPConstant.SERVER_CHAN_QUEUE, message);
     }
 
     @Override
+    @Async
     public void sendBark(String message) {
         rabbitTemplate.convertAndSend(AMQPConstant.BARK_QUEUE, message);
     }
 
     @Override
+    @Async
     public void sendTG(String message) {
         rabbitTemplate.convertAndSend(AMQPConstant.TG_QUEUE, message);
     }
 
     @Override
+    @Async
     public void sendWorkWeixin(String message) {
         rabbitTemplate.convertAndSend(AMQPConstant.WORK_WEIXIN_QUEUE, message);
     }
